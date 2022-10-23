efe3eeeb885fd7b655155cbbfce7ee4666de13bc
barshefet <bar.shefet@gmail.com>
2022-10-23 12:21:59 +0300

    updated the mtf

--- src/server/MTF.ts
+++ src/server/MTF.ts
@@ -1,10 +1,12 @@
 export class MTF{
 roomReady: boolean
+playersReady: []
 phase: number
 admin: string
 
-constructor(roomReady: boolean, phase: number, admin: string){
+constructor(roomReady: boolean, playersReady: [],phase: number, admin: string){
     this.roomReady = roomReady
+    this.playersReady = playersReady
     this.phase = phase
     this.admin = admin
 }
