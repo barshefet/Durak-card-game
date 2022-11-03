6602b01fd39202af9bf27eae3b202407326fec9e
barshefet <bar.shefet@gmail.com>
2022-11-03 10:44:04 +0200

    asd

--- src/client/src/service/socket.ts
+++ src/client/src/service/socket.ts
@@ -1,4 +1,11 @@
 import * as io from "socket.io-client";
 
 
-export const socket = io.connect("http://localhost:4000");
+export const socket = io.connect("https://durak-card-game-barshefet.herokuapp.com/")
+
+// if(process.env.NODE_ENV === "development"){
+//     socket = io.connect("http://localhost:4000");
+// }else{
+//     socket = io.connect("https://durak-card-game-barshefet.herokuapp.com/")
+    
+// }
